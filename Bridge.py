58998c6be719f4ff38db4ce2438ddb7dc87854e1
Akshay Gupta <41187426+AkshayGuptaK@users.noreply.github.com>
2018-11-18 12:01:40 +0530

    Update Bridge.py
    Minor refactoring

--- Bridge.py
+++ Bridge.py
@@ -1,23 +1,23 @@
 from random import randint
 
-def generatenumber():
+def generateNumber():
 	return randint(1,13)
 
-def generatesuit():
+def generateSuit():
 	return randint(0,3)
 		
-def generatecard():
-	return [generatenumber(), generatesuit()]
+def generateCard():
+	return [generateNumber(), generateSuit()]
 	
-def generatehand():
+def generateHand():
 	hand = []
 	while len(hand) < 13:
-		newcard = generatecard()
+		newcard = generateCard()
 		if hand.count(newcard) == 0:
 			hand.append(newcard)
 	return hand
 			
-def convertfacecards(number):
+def convertFaceCards(number):
 	if number == 1:
 		return "Ace"
 	elif number == 11:
@@ -29,7 +29,7 @@ def convertfacecards(number):
 	else:
 		return str(number)
 
-def convertsuit(number):
+def convertSuit(number):
 	if number == 0:
 		return "Spades"
 	elif number == 1:
@@ -41,13 +41,13 @@ def convertsuit(number):
 	else:
 		return "No Trump"
 
-def displayhand(hand):
+def displayHand(hand):
 	for card in hand:
-		name = convertfacecards(card[0])
-		suit = convertsuit(card[1])
+		name = convertFaceCards(card[0])
+		suit = convertSuit(card[1])
 		print("%s of %s" %(name,suit))
 
-def calculatehcp(hand):
+def calculateHighCardPoints(hand):
 	score = 0
 	for card in hand:
 		value = card[0] - 10
@@ -57,13 +57,13 @@ def calculatehcp(hand):
 			score +=4
 	return score
 
-def suitlengths(hand):
+def suitLengths(hand):
 	lengths = [0,0,0,0]
 	for card in hand:
 		lengths[card[1]] +=1
 	return lengths
 
-def determinelargestelementlargerthan(list, minimum):
+def determineLargestElementLargerThan(list, minimum):
 	highestvalue = 0
 	indexofhighestvalue = -1
 	index = 0
@@ -74,26 +74,26 @@ def determinelargestelementlargerthan(list, minimum):
 		index += 1
 	return indexofhighestvalue
 	
-def determine1strengthbid(lengths):
+def determine1StrengthBid(lengths):
 	majors = [lengths[0], lengths[1]]
 	minors = [lengths[2], lengths[3]]
-	suit = determinelargestelementlargerthan(majors, 5)
+	suit = determineLargestElementLargerThan(majors, 5)
 	if suit == -1:
-		return [1, determinelargestelementlargerthan(minors, 3)+2]
+		return [1, determineLargestElementLargerThan(minors, 3)+2]
 	else:
 		return [1, suit]
 
-def determine2strengthbid(lengths):
+def determine2StrengthBid(lengths):
 	del lengths[3]
-	suit = determinelargestelementlargerthan(lengths, 6)
+	suit = determineLargestElementLargerThan(lengths, 6)
 	if suit == -1:
 		return [0, 0]
 	else:
 		return [2, suit]
 	
-def determinebid(hand):
-	hcp = calculatehcp(hand)
-	lengths = suitlengths(hand)
+def determineBid(hand):
+	hcp = calculateHighCardPoints(hand)
+	lengths = suitLengths(hand)
 	if hcp > 27:
 		return [4, 4]
 	elif hcp >= 25:
@@ -101,19 +101,19 @@ def determinebid(hand):
 	elif hcp >= 22:
 		return [2, 3]
 	elif hcp >= 12:
-		return determine1strengthbid(lengths)
+		return determine1StrengthBid(lengths)
 	elif hcp >= 5:
-		return determine2strengthbid(lengths)
+		return determine2StrengthBid(lengths)
 	else:
 		return [0, 0]
 	
-def displaybid(bid):
+def displayBid(bid):
 	if bid[0] == 0:
 		print("Pass")
 	else:
-		suit = convertsuit(bid[1])
+		suit = convertSuit(bid[1])
 		print("Bid is %d %s" %(bid[0],suit))
 	
-hand = generatehand()
-displayhand(hand)
-displaybid(determinebid(hand))
+hand = generateHand()
+displayHand(hand)
+displayBid(determineBid(hand))
